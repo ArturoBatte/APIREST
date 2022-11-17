bc9c2f64d985c7766cd794b883f4cfd71c194b94
Arturo Batte <arturobatte.00800@gmail.com>
2022-11-16 22:39:54 -0300

    Tarea 4 API REST (Sesiones)

--- index.js
+++ index.js
@@ -13,14 +13,9 @@ servidor.get("/", (peti, resp)=>{
     resp.send("Hola desde servidor REST");
 });
 
-<<<<<<< HEAD
 servidor.use ("/libro", require('./rutas/ruta-libro'));
 servidor.use ("/autor", require('./rutas/ruta-autor'));
 servidor.use ("/sesion", require('./rutas/ruta-sesion'));
-=======
-servidor.use("/libro", require('./rutas/ruta-libro'));
-servidor.use("/autor", require('./rutas/ruta-autor'));
->>>>>>> 67d5d8c1888784dfdbb61f368511387267cc2462
 
 servidor.listen(3000, ()=>{
     console.log("Servidor escuchando en el puerto 3000");
